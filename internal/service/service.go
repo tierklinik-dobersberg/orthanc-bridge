e57f95caa19e7bad5b02397e5d56170fdfeee443
Patrick Pacher <patrick.pacher@gmail.com>
2025-02-07 10:28:03 +0100

    Small refactoring

--- internal/service/service.go
+++ internal/service/service.go
@@ -13,6 +13,7 @@ import (
 
 	connect "github.com/bufbuild/connect-go"
 	"github.com/hashicorp/go-multierror"
+	orthanc_bridgev1 "github.com/tierklinik-dobersberg/apis/gen/go/tkd/orthanc_bridge/v1"
 	v1 "github.com/tierklinik-dobersberg/apis/gen/go/tkd/orthanc_bridge/v1"
 	"github.com/tierklinik-dobersberg/apis/gen/go/tkd/orthanc_bridge/v1/orthanc_bridgev1connect"
 	"github.com/tierklinik-dobersberg/apis/pkg/auth"
@@ -98,90 +99,15 @@ func (svc *Service) ListStudies(ctx context.Context, req *connect.Request[v1.Lis
 		return nil, fmt.Errorf("failed to query for studies: %w", err)
 	}
 
-	response := new(v1.ListStudiesResponse)
-
-	for _, r := range res {
-		merr := new(multierror.Error)
-
-		study := &v1.Study{
-			StudyUid:    parseFirstString(r, dicomweb.StudyInstanceUID, merr),
-			Time:        timestamppb.New(parseDateAndTime(r, dicomweb.StudyDate, dicomweb.StudyTime, nil)),
-			Modalities:  parseStringList(r, dicomweb.ModalitiesInStudy, nil),
-			PatientName: parseSingleName(r, dicomweb.PatientName, nil),
-			OwnerName:   parseSingleName(r, dicomweb.ResponsiblePerson, nil),
-			Tags:        parseTags(r),
-		}
-
-		// bail out if there were errors
-		if err := merr.ErrorOrNil(); err != nil {
-			slog.Info("failed to get study", "id", study.StudyUid, "error", err)
-			continue
-		}
-
-		// get series for the study
-		series, err := svc.DICOMWebClient.Query(ctx, dicomweb.QIDORequest{
-			Type:             dicomweb.Series,
-			StudyInstanceUID: study.StudyUid,
-			IncludeFields:    qidoReq.IncludeFields,
-		})
-		if err != nil {
-			slog.Info("failed to fetch study series", "id", study.StudyUid, "error", err)
-			continue
-		}
-
-		for _, s := range series {
-			merr := new(multierror.Error)
-
-			seriesPb := &v1.Series{
-				SeriesUid: parseFirstString(s, dicomweb.SeriesInstanceUID, merr),
-				Time:      timestamppb.New(parseDateAndTime(s, dicomweb.SeriesDate, dicomweb.SeriesTime, nil)),
-				Tags:      parseTags(s),
-			}
-
-			// bail out if there were errors
-			if err := merr.ErrorOrNil(); err != nil {
-				slog.Info("failed to convert series", "id", study.StudyUid, "series", seriesPb.SeriesUid, "error", err)
-				continue
-			}
-
-			instances, err := svc.DICOMWebClient.Query(ctx, dicomweb.QIDORequest{
-				Type:              dicomweb.Instance,
-				StudyInstanceUID:  study.StudyUid,
-				SeriesInstanceUID: seriesPb.SeriesUid,
-				IncludeFields:     qidoReq.IncludeFields,
-			})
-			if err != nil {
-				slog.Info("failed to fetch instances", "id", study.StudyUid, "series", seriesPb.SeriesUid, "error", err)
-				continue
-			}
-
-			for _, i := range instances {
-				merr := new(multierror.Error)
-
-				ipb := &v1.Instance{
-					InstanceUid: parseFirstString(i, dicomweb.SOPInstanceUID, merr),
-					Time:        timestamppb.New(parseDateAndTime(i, dicomweb.InstanceCreationDate, dicomweb.InstanceCreationTime, nil)),
-					Tags:        parseTags(i),
-				}
-
-				if err := merr.ErrorOrNil(); err != nil {
-					slog.Error("failed to convert instance", "id", study.StudyUid, "series", seriesPb.SeriesUid, "instance", ipb.InstanceUid, "error", err)
-					continue
-				}
-
-				seriesPb.Instances = append(seriesPb.Instances, ipb)
-			}
-
-			study.Series = append(study.Series, seriesPb)
-		}
-
-		response.Studies = append(response.Studies, study)
+	studies, err := svc.fetchStudies(ctx, qidoReq)
+	if err != nil {
+		return nil, err
 	}
 
-	// Sort the studies by time
-	sort.Sort(
-		sort.Reverse(StudyListByTime(response.Studies)),
-	)
+	response := &orthanc_bridgev1.ListStudiesResponse{
+		Studies:    studies,
+		TotalCount: int64(len(studies)),
+	}
 
 	if len(res) > 0 && len(response.Studies) == 0 {
 		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to convert response, no studies available"))
@@ -291,4 +217,103 @@ func (svc *Service) ShareStudy(ctx context.Context, req *connect.Request[v1.Shar
 		Token:     token,
 		ViewerUrl: viewerUrl,
 	}), nil
+}
+
+func (svc *Service) fetchStudies(ctx context.Context, qidoReq dicomweb.QIDORequest) ([]*orthanc_bridgev1.Study, error) {
+	res, err := svc.DICOMWebClient.Query(ctx, qidoReq)
+	if err != nil {
+		if re, ok := err.(*dicomweb.ResponseError); ok {
+			body, _ := io.ReadAll(re.Response.Body)
+			slog.Error("failed to query for studies", "error", err, "response", string(body))
+		}
+
+		return nil, fmt.Errorf("failed to query for studies: %w", err)
+	}
+
+	var response []*orthanc_bridgev1.Study
+
+	for _, r := range res {
+		merr := new(multierror.Error)
+
+		study := &v1.Study{
+			StudyUid:    parseFirstString(r, dicomweb.StudyInstanceUID, merr),
+			Time:        timestamppb.New(parseDateAndTime(r, dicomweb.StudyDate, dicomweb.StudyTime, nil)),
+			Modalities:  parseStringList(r, dicomweb.ModalitiesInStudy, nil),
+			PatientName: parseSingleName(r, dicomweb.PatientName, nil),
+			OwnerName:   parseSingleName(r, dicomweb.ResponsiblePerson, nil),
+			Tags:        parseTags(r),
+		}
+
+		// bail out if there were errors
+		if err := merr.ErrorOrNil(); err != nil {
+			slog.Info("failed to get study", "id", study.StudyUid, "error", err)
+			continue
+		}
+
+		// get series for the study
+		series, err := svc.DICOMWebClient.Query(ctx, dicomweb.QIDORequest{
+			Type:             dicomweb.Series,
+			StudyInstanceUID: study.StudyUid,
+			IncludeFields:    qidoReq.IncludeFields,
+		})
+		if err != nil {
+			slog.Info("failed to fetch study series", "id", study.StudyUid, "error", err)
+			continue
+		}
+
+		for _, s := range series {
+			merr := new(multierror.Error)
+
+			seriesPb := &v1.Series{
+				SeriesUid: parseFirstString(s, dicomweb.SeriesInstanceUID, merr),
+				Time:      timestamppb.New(parseDateAndTime(s, dicomweb.SeriesDate, dicomweb.SeriesTime, nil)),
+				Tags:      parseTags(s),
+			}
+
+			// bail out if there were errors
+			if err := merr.ErrorOrNil(); err != nil {
+				slog.Info("failed to convert series", "id", study.StudyUid, "series", seriesPb.SeriesUid, "error", err)
+				continue
+			}
+
+			instances, err := svc.DICOMWebClient.Query(ctx, dicomweb.QIDORequest{
+				Type:              dicomweb.Instance,
+				StudyInstanceUID:  study.StudyUid,
+				SeriesInstanceUID: seriesPb.SeriesUid,
+				IncludeFields:     qidoReq.IncludeFields,
+			})
+			if err != nil {
+				slog.Info("failed to fetch instances", "id", study.StudyUid, "series", seriesPb.SeriesUid, "error", err)
+				continue
+			}
+
+			for _, i := range instances {
+				merr := new(multierror.Error)
+
+				ipb := &v1.Instance{
+					InstanceUid: parseFirstString(i, dicomweb.SOPInstanceUID, merr),
+					Time:        timestamppb.New(parseDateAndTime(i, dicomweb.InstanceCreationDate, dicomweb.InstanceCreationTime, nil)),
+					Tags:        parseTags(i),
+				}
+
+				if err := merr.ErrorOrNil(); err != nil {
+					slog.Error("failed to convert instance", "id", study.StudyUid, "series", seriesPb.SeriesUid, "instance", ipb.InstanceUid, "error", err)
+					continue
+				}
+
+				seriesPb.Instances = append(seriesPb.Instances, ipb)
+			}
+
+			study.Series = append(study.Series, seriesPb)
+		}
+
+		response = append(response, study)
+	}
+
+	// Sort the studies by time
+	sort.Sort(
+		sort.Reverse(StudyListByTime(response)),
+	)
+
+	return response, nil
 }
